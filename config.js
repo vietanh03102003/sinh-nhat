2846d3b8d511d91559fc490d1c2bf489d89685d1
vietanh03102003 <85342856+vietanh03102003@users.noreply.github.com>
2024-10-21 22:49:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Birthday';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const titleCard = 'Dear Thùy Linh';
+const contentCard = 'Chào mừng bạn đã đến với tuổi 22. Tuy thêm 1 tuổi nhưng 22 tuổi mới là thời điểm con gái xinh nhất, hoàn hảo nhất nên bạn phải tự tin với bản thân, đừng sợ sai hãy lấy lại tự tin của những năm 1 năm 2 đi. Chỉ biết chúc bạn sớm học xong IELTS để tập trung vào công việc và việc học chuyên ngành, cuộc sống vui vẻ hơn bớt phiền muộn à cả ngủ sớm hơn nữa. Chúc bạn sẽ có được tất cả điều gì bạn muốn';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
